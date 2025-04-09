2103b8814f94dde8ccafa272ccf9beca62c1913d
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-04-09 09:20:11 +0200

    feat: DataBase added

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pyautogui
+import pandas
 # First Step: Sync the Company’s Server
 # https://dlp.hashtagtreinamentos.com/python/intensivao/login
-  #Open the browser
-pyautogui.PAUSE = 3
+#Open the browser
+pyautogui.PAUSE = 2
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 pyautogui.press("tab")
+#Open Company Site
 pyautogui.press("enter")
 pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
 pyautogui.press('enter')
+#Registration 
 pyautogui.press('tab')
-pyautogui.press('enter')
 pyautogui.write('dev')
 pyautogui.press('tab')
 pyautogui.write('gs12')
 pyautogui.press('tab')
 pyautogui.press('enter')
+#Import DataBase
+pandas.read_csv("products.csv")
+tabela = pandas.read_csv("products.csv")
+print(tabela)
